63e990043237406cdbdc1f2322ead39fea337cf9
Keren Kinberg <kkinberg13@gmail.com>
2021-05-05 14:32:36 +0300

    Testing listItemsController

--- src/routes/__tests__/list-items-controller.exercise.js
+++ src/routes/__tests__/list-items-controller.exercise.js
@@ -1,45 +1,53 @@
 // Testing Controllers
 
 // 🐨 you'll need a few of the generaters from test/utils/generate.js
-// 💰 remember, you can import files in the test/utils directory as if they're node_modules
-// 💰 import * as generate from 'utils/generate'
+import {
+  buildReq,
+  buildRes,
+  buildUser,
+  buildBook,
+  buildListItem,
+} from 'utils/generate'
 
 // 🐨 getListItem calls `expandBookData` which calls `booksDB.readById`
 // so you'll need to import the booksDB from '../../db/books'
-// 💰 import * as booksDB from '../../db/books'
-
+import * as booksDB from '../../db/books'
 // 🐨 don't forget to import the listItemsController from '../list-items-controller'
 // here, that's the thing we're testing afterall :)
-// 💰 import * as listItemsController from '../list-items-controller'
+import * as listItemsController from '../list-items-controller'
 
 // 🐨 use jest.mock to mock '../../db/books' because we don't actually want to make
 // database calls in this test file.
+jest.mock('../../db/books')
 
 // 🐨 ensure that all mock functions have their call history cleared using
 // jest.resetAllMocks here as in the example.
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 test('getListItem returns the req.listItem', async () => {
-  // 🐨 create a user
-  //
-  // 🐨 create a book
-  //
+  const user = buildUser()
+  const book = buildBook()
+
   // 🐨 create a listItem that has the user as the owner and the book
-  // 💰 const listItem = buildListItem({ownerId: user.id, bookId: book.id})
-  //
+  const listItem = buildListItem({ownerId: user.id, bookId: book.id})
+
   // 🐨 mock booksDB.readById to resolve to the book
-  // 💰 use mockResolvedValueOnce
+  booksDB.readById.mockResolvedValueOnce(book)
   //
   // 🐨 make a request object that has properties for the user and listItem
-  // 💰 checkout the implementation of getListItem in ../list-items-controller
-  // to see how the request object is used and what properties it needs.
-  // 💰 and you can use buildReq from utils/generate
-  //
+  const request = buildReq({user, listItem})
   // 🐨 make a response object
-  // 💰 just use buildRes from utils/generate
-  //
+  const response = buildRes()
+
   // 🐨 make a call to getListItem with the req and res (`await` the result)
-  //
+  await listItemsController.getListItem(request, response)
   // 🐨 assert that booksDB.readById was called correctly
-  //
+  expect(booksDB.readById).toHaveBeenCalledTimes(1)
+  expect(booksDB.readById).toHaveBeenCalledWith(book.id)
+
   //🐨 assert that res.json was called correctly
+  expect(response.json).toHaveBeenCalledTimes(1)
+  expect(response.json).toHaveBeenCalledWith({listItem: {...listItem, book}})
 })
